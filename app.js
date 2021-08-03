24c8068f1ad03a1921214b291a94b763faa4e6d5
DanielAdeyemi <adeyemidany@gmail.com>
2021-08-02 20:48:24 -0700

    block scope work

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ function sum(num1, num2) {
 
 let x = sum(2,3);
 console.log(x);
+
+function greeting() {
+  let message = 'Hi';
+  let sayHi = function hi() {
+    let message = 'Hi hi';
+    console.log(message + ' inside block');
+  }
+  sayHi();
+  console.log(message);
+}
+
+greeting();
