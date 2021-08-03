24d698b51c9a4bfb65ed62a4831361ad35244fee
DanielAdeyemi <adeyemidany@gmail.com>
2021-08-02 21:03:19 -0700

    working with closures

--- app.js
+++ app.js
@@ -20,3 +20,27 @@ greeting();
 (function wow() {
   console.log('IIFE');
 })();
+
+let closure = (function() {
+  let message = 'M1';
+  let getMessage = function() {
+    return message;
+  };
+  return {
+    getMessage: getMessage
+  };
+})();
+console.log(closure.getMessage());
+
+function setupCounter(val) {
+  return function counter() {
+    return val++;
+  }
+}
+
+  let counter1 = setupCounter(0);
+  console.log(counter1());
+  console.log(counter1());
+  let counter2 = setupCounter(10);
+  console.log(counter2());
+  console.log(counter2());
