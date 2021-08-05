9fe05fbe1fb003fa8650ecf9f928e4a5a0c0d076
DanielAdeyemi <adeyemidany@gmail.com>
2021-08-04 22:11:03 -0700

    use of spread operator

--- app2.js
+++ app2.js
@@ -37,8 +37,19 @@ let sum1 = (n1, n2) => n1 + n2;
 let output = sum1(2, 4);
 console.log(output);
 
+// rest param ...names will take an array of names and we can loop over it later
+
 let greetRest = (...names) => {
   names.forEach((name, index) => console.log(`${index + 1} :Hi, ${name}!`));
 }
 
 greetRest('Danny', 'Nsisong', 'Eno');
+
+//  Spread is opposite of rest - takes an array as an arragument and assign it to indiv param
+
+let greetSpread = (person1, person2) => {
+  console.log(`Hello ${person1} and ${person2}`);
+}
+
+let namesSpread = ['Eno', 'Nsisong', 'Dany'];
+greetSpread(...namesSpread);
