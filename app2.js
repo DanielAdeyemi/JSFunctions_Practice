f225e6b62824093d0f7e4bdaa92db66b3fc54760
DanielAdeyemi <adeyemidany@gmail.com>
2021-08-04 21:17:55 -0700

    make 2 greet arrow functions

--- app2.js
+++ app2.js
@@ -4,11 +4,26 @@
 
 
 //arrow function
-let greetings = () => {
-  return 'Hello World';
-}
+// let greetings = () => {
+//   return 'Hello World';
+// }
 
-// or we can put let greetings = () => 'Hello World'; because no parameters we are passing
+// or we can put 
+let greetings = () => 'Hello World Arrow'; 
+// because no parameters we are passing
 
 let message = greetings();
 console.log(message);
+
+
+let greet = function greeting(name) {
+  return `Hello, ${name}!`;
+}
+
+let message1 = greet('John');
+console.log(message1);
+
+//arrow function for greet
+let greet2 = name => `Hello ${name}!`;
+let message1a = greet2('John Arrow');
+console.log(message1a);
