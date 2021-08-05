3646855747edcd81da42f76007ba64d4c904d45c
DanielAdeyemi <adeyemidany@gmail.com>
2021-08-04 21:02:31 -0700

    arrow function 1

---
+++ app2.js
@@ -0,0 +1,14 @@
+// let greetings = function() {
+//   return 'Hello World';
+// } usual function
+
+
+//arrow function
+let greetings = () => {
+  return 'Hello World';
+}
+
+// or we can put let greetings = () => 'Hello World'; because no parameters we are passing
+
+let message = greetings();
+console.log(message);
