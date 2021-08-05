359afcbe0678beca05aebf3c513f44dc66b865ec
DanielAdeyemi <adeyemidany@gmail.com>
2021-08-04 21:25:25 -0700

    make sum as arrow function

--- app2.js
+++ app2.js
@@ -27,3 +27,12 @@ console.log(message1);
 let greet2 = name => `Hello ${name}!`;
 let message1a = greet2('John Arrow');
 console.log(message1a);
+
+function sum(n1, n2) {
+  return n1 + n2;
+}
+
+let sum1 = (n1, n2) => n1 + n2;
+
+let output = sum1(2, 4);
+console.log(output);
